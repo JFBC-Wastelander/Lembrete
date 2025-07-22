8f4e762551731e5a9ea89003c942124b11c428d4
Luan <luan.msilva19@gmail.com>
2025-07-22 08:56:25 -0300

    "feat: switch case"

--- Main.js
+++ Main.js
@@ -5,6 +5,45 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+function Bemvindo() {
+    console.log(`
+Bem vindo ao seu lembrete
+1 - Criar um lembrete
+2 - Exibir Lembretes
+3 - Atualizar Lembretes
+4 - Deletar Lembretes
+5 - Sair`)
+}
+
+Bemvindo()
 rl.on('line', (input) =>{
+
+    switch (input) {
+        case '1':
+
+            break;
+
+        case '2':
+
+            break;
+
+        case '3':
+
+            break;
+
+        case '4':
+
+            break;
+
+        case '5':
+
+            break;
+
+
+        default:
+
+        
+            break;
+    }
 
 });
