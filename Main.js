14c448bddefd58b9b6dc0a2f93dfeab90e7405a4
Mazur <gabrielvitormzr@gmail.com>
2025-07-22 11:34:13 -0300

    Feat:codigo finalizado e testado

--- Main.js
+++ Main.js
@@ -1,49 +1,106 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
+let array = [];
+
 function Bemvindo() {
-    console.log(`
+  console.log(`
 Bem vindo ao seu lembrete
 1 - Criar um lembrete
 2 - Exibir Lembretes
 3 - Atualizar Lembretes
 4 - Deletar Lembretes
-5 - Sair`)
+5 - Sair`);
 }
 
-Bemvindo()
-rl.on('line', (input) =>{
+Bemvindo();
+rl.on("line", (input) => {
+  switch (input) {
+    case "1":
+      criar();
+      break;
 
-    switch (input) {
-        case '1':
+    case "2":
+      exibir();
+      break;
 
-            break;
+    case "3":
+      atualizar();
+      break;
 
-        case '2':
+    case "4":
+      deletar();
+      break;
 
-            break;
+    case "5":
+      console.log("Saindo . . .");
+      process.exit();
 
-        case '3':
-
-            break;
-
-        case '4':
-
-            break;
-
-        case '5':
-
-            break;
-
-
-        default:
-
-        
-            break;
-    }
-
+    default:
+      console.log("Escreva corretamente");
+      Bemvindo();
+      break;
+  }
 });
+
+function criar() {
+  rl.question("Qual lembrete deseja criar ? ", (lembrete) => {
+    rl.question("Qual a data do seu lembrete ? ", (data) => {
+      rl.question("Qual o Andamento: ", (andamento) => {
+        const lembretes = {
+          lembrete,
+          data,
+          andamento,
+        };
+        array.push(lembretes);
+        console.log("Lembrete criado com sucesso");
+        Bemvindo();
+      });
+    });
+  });
+}
+
+function exibir() {
+  array.forEach((a, i) => {
+    console.log(`${i + 1} - ${a.lembrete}, ${a.data}, ${a.andamento}`);
+  });
+  Bemvindo();
+}
+
+function atualizar() {
+  array.forEach((a, i) => {
+    console.log(`${i + 1} - ${a.lembrete}, ${a.data}, ${a.andamento}`);
+  });
+  rl.question("Qual lembrete deseja alterar? ", (inputEditar) => {
+    let numero = inputEditar - 1;
+    rl.question("Qual o novo lembrete ? ", (lembrete) => {
+      rl.question("Qual a data do seu lembrete ? ", (data) => {
+        rl.question(" Qual o Andamento: ", (andamento) => {
+          array[numero] = {
+            lembrete,
+            data,
+            andamento,
+          };
+          console.log("Seu lembrete foi atualizado com sucesso! ");
+          Bemvindo();
+        });
+      });
+    });
+  });
+}
+
+function deletar() {
+  array.forEach((a, i) => {
+    console.log(`${i + 1} - ${a.lembrete}, ${a.data}, ${a.andamento}`);
+  });
+  rl.question("Qual lembrete deseja deletar ?", (input) => {
+    let numero = input - 1;
+    array.splice(numero, 1);
+    console.log("Lembrete deletado com sucesso");
+  });
+  Bemvindo();
+}
