a5e6d4ee399bd3a0f2e83e929042ff1c1fb01f76
Luan <luan.msilva19@gmail.com>
2025-07-26 13:30:08 -0300

    feat: concluir lembrete

--- Main.js
+++ Main.js
@@ -14,7 +14,8 @@ Bem vindo ao seu lembrete
 2 - Exibir Lembretes
 3 - Atualizar Lembretes
 4 - Deletar Lembretes
-5 - Sair`);
+5 - Marcar como concluido
+6 - Sair`);
 }
 
 Bemvindo();
@@ -37,6 +38,10 @@ rl.on("line", (input) => {
       break;
 
     case "5":
+      concluido();
+      break;
+
+    case "6":
       console.log("Saindo . . .");
       process.exit();
 
@@ -103,4 +108,25 @@ function deletar() {
     console.log("Lembrete deletado com sucesso");
   });
   Bemvindo();
+}
+
+function concluido() {
+
+  if (array.length === 0) {
+    console.log("Você não tem lembretes para concluir.")
+    Bemvindo()
+  }
+  console.log("====== Lembretes para concluir ======")
+  array.forEach((a, i) => {
+    console.log(`${i + 1} - ${a.lembrete}, ${a.data}, ${a.andamento}`);
+  });
+
+  rl.question("Qual lembrete deseja marcar como concluido?", (concluir) => {
+    const index = parseInt(concluir) - 1
+
+    array[index].andamento = "Conluido"
+    console.log("O lembrete foi concluido!")
+    Bemvindo()
+  } )
+  
 }
